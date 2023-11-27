b949863672cdc4fba08a867a21abc17b38251092
Carlos Andrés Ruiz Hinestroza <123417384+carlosandresgoo@users.noreply.github.com>
2023-11-27 18:10:37 -0500

    avances en el inicio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,21 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 //módulos de componentes
 import { AppComponent } from './app.component';
+import { Iniciocomponents } from './components/inicio/inicio.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
-import { SobreMiComponent } from './components/sobre-mi/sobre-mi.component';
 import { ProyectosComponent } from './components/proyectos/proyectos.component';
 import { AppRoutingModule } from './app-routing.module';
 import { ContactoComponent } from './components/contacto/contacto.component';
+import { SobreMiComponent } from './components/sobre-mi/sobre-mi.component';
+
 
 // módulos de angular materials
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
-    SobreMiComponent,
+    Iniciocomponents,
     ProyectosComponent,
-    ContactoComponent
+    ContactoComponent,
+    SobreMiComponent
   ],
   imports: [
     BrowserModule,
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -8,73 +8,75 @@ import { Router } from '@angular/router';
 })
 export class NavbarComponent {
 
-  navbarScrolled: boolean = false;
+navbarScrolled: boolean = false;
 
-  constructor(private el: ElementRef, private router : Router) {}
+constructor(private el: ElementRef, private router: Router) {}
 
-  ngOnInit() {
+ngOnInit() {
+  this.removeScrolledStyles();
+}
+
+@HostListener('window:scroll', [])
+onWindowScroll() {
+  const scrollPosition =
+    window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+  this.navbarScrolled = scrollPosition > 80;
+
+  if (this.navbarScrolled) {
+    this.applyScrolledStyles();
+  } else {
     this.removeScrolledStyles();
   }
+}
 
-  @HostListener('window:scroll', [])
-  onWindowScroll() {
-    const scrollPosition = window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
-    this.navbarScrolled = scrollPosition > 550;
+applyScrolledStyles() {
+  const navbarElement = this.el.nativeElement.querySelector('.custom-navbar');
 
-    if (this.navbarScrolled) {
-      this.applyScrolledStyles();
-    } else {
-      this.removeScrolledStyles();
-    }
+  if (navbarElement && !navbarElement.classList.contains('fixed-navbar')) {
+    navbarElement.classList.add('fixed-navbar');
+    navbarElement.style.backgroundColor = 'white';
+    navbarElement.classList.add('show');
   }
 
-  applyScrolledStyles() {
-    const navbarElement = this.el.nativeElement.querySelector('.custom-navbar');
+  const links = this.el.nativeElement.querySelectorAll('.nav-link');
+  const h2 = this.el.nativeElement.querySelector('h2');
 
-    if (navbarElement) {
-      navbarElement.style.backgroundColor = 'white';
-    }
-
-    const links = this.el.nativeElement.querySelectorAll('.nav-link');
-    const h2 = this.el.nativeElement.querySelector('h2');
-
-    if (links) {
-      links.forEach((link: { style: { color: string; margin: string; }; }) => {
-        link.style.color = 'black';
-        link.style.margin = '0'; // Agregar margen cero solo cuando haces scroll
-      });
-    }
-
-    if (h2) {
-      h2.style.color = 'black';
-      h2.style.margin = '0'; // Agregar margen cero solo cuando haces scroll
-    }
+  if (links) {
+    links.forEach((link: { style: { color: string; marginTop: string; marginRight: string } }) => {
+      link.style.color = 'black';
+      link.style.marginTop = '0';
+    });
   }
 
-  removeScrolledStyles() {
-    const navbarElement = this.el.nativeElement.querySelector('.custom-navbar');
+  if (h2) {
+    h2.style.color = 'black';
+    h2.style.marginTop = '0';
+  }
+}
 
-    if (navbarElement) {
-      navbarElement.style.backgroundColor = 'rgba(255, 255, 255, 0.01)';
-    }
+removeScrolledStyles() {
+  const navbarElement = this.el.nativeElement.querySelector('.custom-navbar');
 
-    const links = this.el.nativeElement.querySelectorAll('.nav-link');
-    const h2 = this.el.nativeElement.querySelector('h2');
-
-    if (links) {
-      links.forEach((link: { style: { color: string; margin: string; }; }) => {
-        link.style.color = 'white';
-        link.style.margin = ''; // Restaurar el margen original
-      });
-    }
-
-    if (h2) {
-      h2.style.color = 'white';
-      h2.style.margin = ''; // Restaurar el margen original
-    }
-
+  if (navbarElement && navbarElement.classList.contains('fixed-navbar')) {
+    navbarElement.classList.remove('fixed-navbar');
+    navbarElement.style.backgroundColor = 'rgba(255, 255, 255, 0.01)';
+    navbarElement.classList.remove('show');
   }
 
+  const links = this.el.nativeElement.querySelectorAll('.nav-link');
+  const h2 = this.el.nativeElement.querySelector('h2');
 
-  
+  if (links) {
+    links.forEach((link: { style: { color: string; marginTop: string; marginRight: string } }) => {
+      link.style.color = 'white';
+      link.style.marginTop = '';
+    });
+  }
+
+  if (h2) {
+    h2.style.color = 'white';
+    h2.style.marginTop = '';
+  }
+}
+
 }
