8e0399dc8b4d5e9962c65a05cb420b9bcb8fd6b9
Carlos Andrés Ruiz Hinestroza <123417384+carlosandresgoo@users.noreply.github.com>
2023-11-28 21:59:42 -0500

    Avances día 3

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,82 +1,58 @@
-import { Component, ElementRef, HostListener, } from '@angular/core';
-import { Router } from '@angular/router';
+  import { Component, ElementRef, HostListener, } from '@angular/core';
+  import { Router } from '@angular/router';
 
-@Component({
-  selector: 'app-navbar',
-  templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
-})
-export class NavbarComponent {
+  @Component({
+    selector: 'app-navbar',
+    templateUrl: './navbar.component.html',
+    styleUrls: ['./navbar.component.css']
+  })
+  export class NavbarComponent {
 
-navbarScrolled: boolean = false;
+    navbarScrolled: boolean = false;
 
-constructor(private el: ElementRef, private router: Router) {}
-
-ngOnInit() {
-  this.removeScrolledStyles();
-}
-
-@HostListener('window:scroll', [])
-onWindowScroll() {
-  const scrollPosition =
-    window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
-  this.navbarScrolled = scrollPosition > 80;
-
-  if (this.navbarScrolled) {
-    this.applyScrolledStyles();
-  } else {
-    this.removeScrolledStyles();
+    constructor(private el: ElementRef) { }
+  
+    ngOnInit() {
+      this.updateNavbarStyles();
+    }
+  
+    @HostListener('window:scroll', [])
+    onWindowScroll() {
+      const scrollPosition =
+        window.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop || 0;
+      this.navbarScrolled = scrollPosition > 80;
+  
+      this.updateNavbarStyles();
+    }
+  
+    updateNavbarStyles() {
+      const navbarElement = this.el.nativeElement.querySelector('.custom-navbar');
+  
+      if (navbarElement) {
+        if (this.navbarScrolled) {
+          navbarElement.style.background = 'rgb(156, 155, 155)';
+          navbarElement.style.background = 'radial-gradient(circle, rgba(156,155,155,1) 1%, rgba(162,161,162,1) 23%, rgba(173,173,174,1) 66%)';
+        } else {
+          navbarElement.style.background = 'rgba(255, 255, 255, 0.01)';
+        }
+      }
+  
+      const links = this.el.nativeElement.querySelectorAll('.nav-link');
+      const h2 = this.el.nativeElement.querySelector('h2');
+  
+      const color = this.navbarScrolled ? 'black' : 'white';
+      const marginTop = this.navbarScrolled ? '0' : '';
+  
+      if (links) {
+        links.forEach((link: { style: { color: string; marginTop: string } }) => {
+          link.style.color = color;
+          link.style.marginTop = marginTop;
+        });
+      }
+  
+      if (h2) {
+        h2.style.color = color;
+        h2.style.marginTop = marginTop;
+      }
+    }
   }
-}
-
-applyScrolledStyles() {
-  const navbarElement = this.el.nativeElement.querySelector('.custom-navbar');
-
-  if (navbarElement && !navbarElement.classList.contains('fixed-navbar')) {
-    navbarElement.classList.add('fixed-navbar');
-    navbarElement.style.backgroundColor = 'white';
-    navbarElement.classList.add('show');
-  }
-
-  const links = this.el.nativeElement.querySelectorAll('.nav-link');
-  const h2 = this.el.nativeElement.querySelector('h2');
-
-  if (links) {
-    links.forEach((link: { style: { color: string; marginTop: string; marginRight: string } }) => {
-      link.style.color = 'black';
-      link.style.marginTop = '0';
-    });
-  }
-
-  if (h2) {
-    h2.style.color = 'black';
-    h2.style.marginTop = '0';
-  }
-}
-
-removeScrolledStyles() {
-  const navbarElement = this.el.nativeElement.querySelector('.custom-navbar');
-
-  if (navbarElement && navbarElement.classList.contains('fixed-navbar')) {
-    navbarElement.classList.remove('fixed-navbar');
-    navbarElement.style.backgroundColor = 'rgba(255, 255, 255, 0.01)';
-    navbarElement.classList.remove('show');
-  }
-
-  const links = this.el.nativeElement.querySelectorAll('.nav-link');
-  const h2 = this.el.nativeElement.querySelector('h2');
-
-  if (links) {
-    links.forEach((link: { style: { color: string; marginTop: string; marginRight: string } }) => {
-      link.style.color = 'white';
-      link.style.marginTop = '';
-    });
-  }
-
-  if (h2) {
-    h2.style.color = 'white';
-    h2.style.marginTop = '';
-  }
-}
-
-}
--- src/app/components/sobre-mi/sobre-mi.component.ts
+++ src/app/components/sobre-mi/sobre-mi.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PdfService } from 'src/app/services/pdf.service';
 
 @Component({
   selector: 'app-sobre-mi',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./sobre-mi.component.css']
 })
 export class SobreMiComponent {
-
+  constructor(public pdfService: PdfService) {}
+  
 }
